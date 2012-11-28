7076c9b91fa5e2be4d977fc236741e50ac772ec0
Daniel <no@reply.com>
2012-11-28 14:46:01 +0100

    changed not needed args control

--- RSAKeyCreation.java
+++ RSAKeyCreation.java
@@ -2,7 +2,7 @@ import crypt.RSA;
 
 public class RSAKeyCreation {
 	public static void main(String[] args){
-		if(args.length == 1 && !args[0].isEmpty()){
+		if(args.length == 1){
 			System.out.println("Owner of keypair: "+args[0]);
 			String ownerName = args[0];
 			RSA rsa = RSA.create(1024);
