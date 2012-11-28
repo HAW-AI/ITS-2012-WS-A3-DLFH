ac86c5dbb12aaa63dae1f2cdcd060a536a3e922a
Daniel <no@reply.com>
2012-11-28 14:38:24 +0100

    corrected test for aes

--- tests/Tests.java
+++ tests/Tests.java
@@ -1,8 +1,6 @@
 package tests;
 import static org.junit.Assert.*;
 
-import javax.crypto.SecretKey;
-
 import org.junit.Test;
 
 import crypt.AES;
@@ -14,10 +12,7 @@ public class Tests {
 	public void testAes128() {
 		String plainData = "Test String";
 		AES aes = AES.create(128);
-		SecretKey skey = aes.generateKey();
-		byte [] encData = aes.encrypt(plainData, skey);
-		byte[] raw_key = aes.keyToByteArray(skey);
-		String decData = aes.decrypt(encData, raw_key);
+		String decData = aes.decrypt(aes.encrypt(plainData));
 		assertTrue(decData.equals(plainData));
 	}
 }
