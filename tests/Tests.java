333c392d359e8dbf30ab3604f9cd76563ba18ff8
Daniel <no@reply.com>
2012-11-28 21:21:20 +0100

    added RSA sign and verify test

--- tests/Tests.java
+++ tests/Tests.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import crypt.AES;
+import crypt.RSA;
 
 
 public class Tests {
@@ -14,5 +15,13 @@ public class Tests {
 		AES aes = AES.create(128);
 		String decData = aes.decrypt(aes.encrypt(plainData));
 		assertTrue(decData.equals(plainData));
+	}
+	
+	@Test
+	public void testRSASign() {
+		AES aes = AES.create(128);
+		RSA rsa = RSA.create(1024);
+		byte[] signatur = rsa.signAesKey(aes.getSkey());
+		assertTrue(rsa.verifyAesKey(aes.getSkey().getEncoded(), signatur));
 	}
 }
