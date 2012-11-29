55e01b5db5c1c15a25e35c44501940fb6ee11386
Daniel <no@reply.com>
2012-11-29 13:00:39 +0100

    changed exit of application to System.exit(0)

--- RSAKeyCreation.java
+++ RSAKeyCreation.java
@@ -2,13 +2,13 @@ import crypt.RSA;
 
 public class RSAKeyCreation {
 	public static void main(String[] args){
-		if(args.length == 1){
-			System.out.println("Owner of keypair: "+args[0]);
-			String ownerName = args[0];
-			RSA rsa = RSA.create(1024);
-			rsa.writeKeysToFile("", "", ownerName);
-		} else {
+		if(args.length < 1){
 			System.out.println("Please enter an owner name as first argument");
+			System.exit(0);
 		}
+		System.out.println("Owner of keypair: "+args[0]);
+		String ownerName = args[0];
+		RSA rsa = RSA.create(1024);
+		rsa.writeKeysToFile("", "", ownerName);
 	}
 }
