c4dbde654375974c9e907db1d838305d50b493ae
Daniel <no@reply.com>
2012-11-29 14:40:26 +0100

    changed io to new io classes

--- ReceiveSecureFile.java
+++ ReceiveSecureFile.java
@@ -1,12 +1,7 @@
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import javax.crypto.SecretKey;
+
+import util.Input;
+import util.Output;
 
 import crypt.AES;
 import crypt.RSA;
@@ -14,53 +9,35 @@ import crypt.RSA;
 
 public class ReceiveSecureFile {
 	public static void main(String[] args){
-		if(args.length == 4){
-			String prvKeyPath = args[0];
-			String pubKeyPath = args[1];
-			String encDataPath = args[2];
-			String outputDataPath = args[3];
-			try {							
-				File ssfFile = new File(encDataPath);
-				FileInputStream ssfFis = new FileInputStream(ssfFile);
-				DataInputStream ssfDis = new DataInputStream(ssfFis);
-				int skeyLength = ssfDis.readInt();
-				byte[] skeyBuffer = new byte[skeyLength];
-				ssfDis.read(skeyBuffer);
-				
-				int sigLength = ssfDis.readInt();
-				byte[] sigBuffer = new byte[sigLength];
-				ssfDis.read(sigBuffer);
-				
-				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				while(ssfDis.available() != 0){
-					baos.write(ssfDis.readByte());
-				}
-				System.out.println("Read data "+baos.toString());
-				ssfDis.close();
-
-				RSA rsa = RSA.create(pubKeyPath, prvKeyPath);
-				SecretKey aesKey = rsa.decryptAesKey(skeyBuffer);
-				AES aes = AES.create(aesKey);
-				byte[] decData = aes.decrypt(baos.toByteArray());
-				
-				File dataFile = new File(outputDataPath);
-				FileOutputStream dataFis = new FileOutputStream(dataFile);
-				DataOutputStream dataDis = new DataOutputStream(dataFis);
-				dataDis.write(decData);
-				dataDis.close();
-				System.out.println(new String(decData));
-				
-				if(rsa.verifyAesKey(aesKey.getEncoded(), sigBuffer)){
-					System.out.println("Verification successfull");
-				} else {
-					System.out.println("Verification failed");
-				}
-			} catch (IOException e) {
-				System.out.println("An error occurred while writing ssf file");
-				e.printStackTrace();
-			}
-		} else {
+		if(args.length < 4){
 			System.out.println("Please enter 4 argument: <privat key> <public key> <plain data> <output file>");
+			System.exit(0);
+		}
+		String prvKeyPath = args[0];
+		String pubKeyPath = args[1];
+		String encDataPath = args[2];
+		String outputDataPath = args[3];					
+		
+		Input in = Input.create(encDataPath);
+		byte[] encSkey = in.readByLength(in.readLength());
+		byte[] signature = in.readByLength(in.readLength());
+		byte[] endData = in.readRemaining();
+		in.close();
+
+		RSA rsa = RSA.create(pubKeyPath, prvKeyPath);
+		SecretKey aesKey = rsa.decryptAesKey(encSkey);
+		AES aes = AES.create(aesKey);
+		byte[] decData = aes.decrypt(endData);
+		
+		Output out = Output.create(outputDataPath);
+		out.write(decData);
+		out.close();
+		System.out.println("Read Data: "+new String(decData));
+		
+		if(rsa.verifyAesKey(aesKey.getEncoded(), signature)){
+			System.out.println("Verification successfull");
+		} else {
+			System.out.println("Verification failed");
 		}
 	}
 }
