786c6e62d3e169d67c11f6ba01de60acc77173cd
Daniel <no@reply.com>
2012-11-29 14:29:29 +0100

    new input and output classes to bundle io functionality

---
+++ util/Input.java
@@ -0,0 +1,72 @@
+package util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Input {
+	
+	DataInputStream dis;
+	
+	private Input(DataInputStream dis){
+		this.dis = dis;
+	}
+	
+	public static Input create(String FilePath){
+		DataInputStream dis = null;
+		try {
+			dis = new DataInputStream(new FileInputStream(new File(FilePath)));
+		} catch (FileNotFoundException e) {
+			System.out.println("File could not be found");
+			e.printStackTrace();
+		}
+		return new Input(dis);
+	}
+
+	public int readLength(){
+		int length = -1;
+		try {
+			length = dis.readInt();
+		} catch (IOException e) {
+			System.out.println("Could not read length from file");
+			e.printStackTrace();
+		}
+		return length;
+	}
+	
+	public byte[] readByLength(int length){
+		byte[] buffer = new byte[length];
+		try {
+			dis.read(buffer);
+		} catch (IOException e) {
+			System.out.println("Could not read data from file");
+			e.printStackTrace();
+		}
+		return buffer;
+	}
+	
+	public byte[] readRemaining(){
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+			while(dis.available() != 0){
+				baos.write(dis.readByte());
+			}
+		} catch (IOException e) {
+			System.out.println("Could not read data from file");
+			e.printStackTrace();
+		}
+		return baos.toByteArray();
+	}
+	
+	public void close(){
+		try {
+			dis.close();
+		} catch (IOException e) {
+			System.out.println("Could not close file");
+			e.printStackTrace();
+		}
+	}
+}
---
+++ util/Output.java
@@ -0,0 +1,54 @@
+package util;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Output {
+	
+	DataOutputStream dos;
+	
+	private Output(DataOutputStream dos){
+		this.dos = dos;
+	}
+	
+	public static Output create(String filePath){
+		DataOutputStream dos = null;
+		try {
+			dos = new DataOutputStream(new FileOutputStream(new File(filePath)));
+		} catch (FileNotFoundException e) {
+			System.out.println("File could not be found");
+			e.printStackTrace();
+		}
+		return new Output(dos);
+	}
+
+	public void writeInt(int value){
+		try {
+			dos.writeInt(value);
+		} catch (IOException e) {
+			System.out.println("Could not write integer to file");
+			e.printStackTrace();
+		}
+	}
+	
+	public void write(byte[] data){
+		try {
+			dos.write(data);
+		} catch (IOException e) {
+			System.out.println("Could not read data from file");
+			e.printStackTrace();
+		}
+	}
+	
+	public void close(){
+		try {
+			dos.close();
+		} catch (IOException e) {
+			System.out.println("Could not close file");
+			e.printStackTrace();
+		}
+	}
+}
